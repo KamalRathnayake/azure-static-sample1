184bbf3a7ca74d44352bf381a4f8e6c1a8eacd72
Kamal Rathnayake <kamal.rathnayake@parkly.no>
2021-10-28 11:31:19 +0530

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'staticsample';
+  public name: string = "";
+  public result: string = ""
+
+  constructor(private http:HttpClient){
+
+  }
+
+  public keyup(){
+    this.http.get('https://blue-mushroom-079053c00.azurestaticapps.net/api/HttpTrigger1?name=kamal').subscribe(resp=>{
+      console.log(resp)
+    })
+  }
 }
